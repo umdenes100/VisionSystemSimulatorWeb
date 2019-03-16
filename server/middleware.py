0cfb94ce083cea1386f0cd4764524ae868d25a8f
mram <mram@umd.edu>
2019-03-16 11:51:07 -0400

    Randomization works

--- server/middleware.py
+++ server/middleware.py
@@ -1,20 +1,35 @@
 from asyncio import create_subprocess_exec, subprocess, get_event_loop
 import websockets
+import json
 
-COMMAND = './randomization/randomize'
+RANDOMIZE = './randomization/randomize'
+SIMULATE = './simulator/simulate'
 
+async def process_command(command, data=None):
+	process = await create_subprocess_exec(*command.split(), 
+										   stdin=subprocess.PIPE, 
+										   stdout=subprocess.PIPE)
+	if data is None:
+		stdout, stderr = await process.communicate()
+	else:
+		stdout, stderr = await process.communicate(input=bytes(data, encoding='utf-8'))
 
-async def generate_simulation(command, data):
-	process = await create_subprocess_exec(*command.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-	# stdout, stderr = await process.communicate(input=bytes(data, encoding='utf-8'))
-	stdout, stderr = await process.communicate()
 	return stdout.decode()
-
 
 async def middleware(websocket, path):
 	async for message in websocket:
-		print(f'Input: {message}')
-		result = await generate_simulation(COMMAND, message)
+
+		print(f'Message: {message}')
+
+		message = json.loads(message)
+		if message['type'] == 'randomization':
+			result = await process_command(RANDOMIZE)
+		elif message['type'] == 'simulation':
+			print(f"Input: {message['content']}")
+			result = await generate_simulation(SIMULATE, message['content'])
+		else:
+			raise ValueError('Unexpected JSON type.')
+
 		print(f'Output: {result}')
 		await websocket.send(result)
 		print()

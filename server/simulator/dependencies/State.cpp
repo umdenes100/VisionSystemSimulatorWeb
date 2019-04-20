1e9f3c360a02e9d8caa1577a22428062de343d15
Karam Singh <karamsingh175@gmail.com>
2019-04-19 20:45:38 -0400

    moved delay function and enabled serial events

--- server/simulator/dependencies/State.cpp
+++ server/simulator/dependencies/State.cpp
@@ -5,7 +5,14 @@ void State::init(char *json)
     // do what we want
 }
 
-void State::update(VisionSystemClient Enes100, Tank tank)
+void State::update(VisionSystemClient Enes100, Tank tank, SerialClient Serial)
 {
     // do what we want
+}
+
+extern State lib_state;
+
+void delay(int msec) 
+{
+    lib_state.init("");
 }
--- server/simulator/dependencies/State.h
+++ server/simulator/dependencies/State.h
@@ -2,13 +2,16 @@
 #define State_hpp
 
 #include "VisionSystemClient.hpp"
+#include "ArduinoHelpers.hpp"
 #include "Tank.h"
 
 class State
 {
 public:
   void init(char *json);
-  void update(VisionSystemClient Enes100, Tank tank);
+  void update(VisionSystemClient Enes100, Tank tank, SerialClient Serial);
 };
+
+void delay(int msec);
 
 #endif /* State.hpp */

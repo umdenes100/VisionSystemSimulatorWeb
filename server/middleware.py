abbeef0340991cf41104853eec6bcc515b9b0023
mram <mram@umd.edu>
2019-04-22 21:40:32 -0400

    Cleanup

--- server/middleware.py
+++ server/middleware.py
@@ -87,7 +87,6 @@ async def middleware(websocket, path):
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', type=str, default='0.0.0.0', help="e.g. 0.0.0.0")
     parser.add_argument('--port', type=int, default=8888, help="e.g. 8888")

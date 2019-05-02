80ae22e8bbaed7a8340b62ddc86caec2c6d92256
mram <mram@umd.edu>
2019-05-02 14:45:20 -0400

    Modified client to work w http

--- client/communication.js
+++ client/communication.js
@@ -2,47 +2,29 @@ let randomization = undefined
 let simulation = undefined
 let editor = undefined
 
+const SERVER_URL = 'http://18.191.246.34:8888'
+const LOCAL_URL = 'http://127.0.0.1:8888'
+
+
+function request_randomization() {
+    let request = { type: 'randomization' }
+    $.get(SERVER_URL, request, data => {
+        let canvasses = [pcanvas, canvas]
+
+        canvasses.map(canv => {
+            canv.obstacles = data.obstacles.map(obstacle => new Obstacle(obstacle.x, obstacle.y))
+            canv.destination = new Destination(data.destination.x, data.destination.y)
+            canv.draw()
+        })
+    })
+}
+
 $(document).ready(() => {
 
-	// const connection = new WebSocket("ws://18.191.246.34:8888/")
-	const connection = new WebSocket("ws://127.0.0.1:8888/")
 
-	connection.onopen = () => {
-		console.log('OPEN')
-		connection.send(JSON.stringify(randomization_request))
-	}
+	request_randomization()
 
-	connection.onerror = error => {
-	    console.log(`WebSocket Error.: ${error}`)
-	}
 
-	connection.onmessage = message => {
-
-		message = JSON.parse(message.data)
-		console.log(message)
-		if (message.type === 'randomization') {
-
-			let $textarea = $('#terminal');
-			$textarea.append('Updated randomization.\n')
-			let last_thousand_messages = $textarea.val().split('\n').slice(-1000).join('\n')
-			$textarea.empty()
-			$textarea.append(last_thousand_messages)
-			$textarea.scrollTop($textarea[0].scrollHeight);
-
-			randomization = message
-
-			canvas.obstacles = message.obstacles.map(obstacle => new Obstacle(obstacle.x, obstacle.y))
-			canvas.destination = new Destination(message.destination.x, message.destination.y)
-			canvas.draw()
-
-		} else {
-			console.log('Unimplemented')
-		}
-	}
-
-	connection.onclose = () => {
-		console.log('Failed')
-	}
 
 	editor = CodeMirror(document.getElementById('code-editor'), {
 		value: 'void setup() {\n\n}\n\nvoid loop() {\n\n}',
@@ -57,9 +39,7 @@ $(document).ready(() => {
 		lineWrapping: true,
 	})
 
-	$('#randomize').on('click', () => {
-		connection.send(JSON.stringify(randomization_request))
-	})
+	$('#randomize').on('click', request_randomization)
 
 	$('#simulate').on('click', () => {
 
@@ -67,14 +47,15 @@ $(document).ready(() => {
 		r.osv.height = 0.3
 		r.osv.width = 0.3
 
-		let simulation_request = JSON.stringify({
+		let simulation_request = {
 			type: 'simulation',
 			code: editor.getDoc().getValue(),
 			randomization: r,
 			distance_sensors: [0, 2],
+		}
+		$.get(SERVER_URL, simulation_request, data => {
+			console.log(data)
 		})
-
-		connection.send(simulation_request)
 	})
 
 })

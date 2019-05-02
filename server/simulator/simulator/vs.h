3cc58983d9f41cd35fe3b550dcf6fe27dcf4b6c4
Karam Singh <karamsingh175@gmail.com>
2019-05-01 20:12:01 -0400

    added arena structs

--- server/simulator/simulator/vs.h
+++ server/simulator/simulator/vs.h
@@ -7,4 +7,26 @@
 void init(char *json);
 struct node * frame(struct node *in, struct process p);
 
+struct coordinate {
+    float x;
+    float y;
+    float theta;
+};
+
+struct obstacle {
+    struct coordinate location;
+    float width;
+    float height;
+};
+
+struct osv {
+    struct coordinate location;
+    int *distance_sensors;
+};
+
+struct arena {
+    struct obstacle *obstacles;
+    struct osv osv;
+};
+
 #endif

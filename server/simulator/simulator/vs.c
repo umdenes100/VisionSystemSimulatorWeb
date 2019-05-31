cf8e31c58bec719455c1e5fe4e9ed90e1f469ccc
Karam Singh <karamsingh175@gmail.com>
2019-05-31 19:25:37 -0400

    fixed left-right pwm issue

--- server/simulator/simulator/vs.c
+++ server/simulator/simulator/vs.c
@@ -577,9 +577,6 @@ int check_for_collisions(struct arena *arena) {
             for(k = 0; k < 4; k++) {
                 struct coordinate *res = get_intersection(osv_sides[k], obstacle_sides[j]);
                 if(res != NULL) {
-                    FILE* fp = fopen("collision.test", "w");
-                    fprintf(fp, "collided osv side %d with obstacle %d at coordinate (%f,%f)\n", k, i, res->x, res->y);
-                    fclose(fp);
                     free(res);
                     return 1;
                 }
@@ -647,7 +644,7 @@ void update_osv(struct arena *arena, int frame_no) {
     arena->osv.location.x = arena->osv.location.x + dist_traveled * cos(arena->osv.location.theta);
     arena->osv.location.y = arena->osv.location.y + dist_traveled * sin(arena->osv.location.theta);
 
-    arena->osv.location.theta += RAD_PER_FRAME * ((arena->osv.right_motor_pwm - arena->osv.left_motor_pwm) / 510.0f);
+    arena->osv.location.theta -= RAD_PER_FRAME * ((arena->osv.right_motor_pwm - arena->osv.left_motor_pwm) / 510.0f);
 
     if(arena->osv.location.theta > PI) {
         arena->osv.location.theta -= 2 * PI;

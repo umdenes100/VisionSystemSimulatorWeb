94eeef1b61e488060ca1822b8950e809456e7ccc
Karam Singh <karamsingh175@gmail.com>
2019-06-10 11:54:32 -0400

    fixed json from error.c

--- server/simulator/simulator/error.c
+++ server/simulator/simulator/error.c
@@ -5,7 +5,7 @@
 
 // this is the standard error function. exits with code "return_code".
 void error(char *error_msg, int return_code) {
-    fprintf(stderr, "Error: {\r\n\t'msg': '%s',\r\n\t'code': '%d'\r\n}\r\n", error_msg, return_code);
+    fprintf(stderr, "{\r\n\t\"msg\": \"%s\",\r\n\t\"code\": \"%d\"\r\n}\r\n", error_msg, return_code);
     fflush(stderr);
     fflush(stdout);
     exit(return_code);

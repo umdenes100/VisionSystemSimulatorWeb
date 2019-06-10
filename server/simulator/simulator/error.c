df961693aa271c184cd24e87bece4409460c5e1b
Karam Singh <karamsingh175@gmail.com>
2019-06-10 12:15:30 -0400

    switched to cJSON for error

--- server/simulator/simulator/error.c
+++ server/simulator/simulator/error.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cjson/cJSON.h>
 
 #include "error.h"
 
 // this is the standard error function. exits with code "return_code".
 void error(char *error_msg, int return_code) {
-    fprintf(stderr, "{\r\n\t\"msg\": \"%s\",\r\n\t\"code\": \"%d\"\r\n}\r\n", error_msg, return_code);
+    
+    cJSON *root = cJSON_CreateObject();
+    cJSON *msg = cJSON_CreateString(error_msg);
+    cJSON_AddItemToObject(root, "msg", msg);
+    cJSON_AddNumberToObject(root, "code", return_code);
+    fprintf(stderr, "%s", cJSON_Print(root));
+    cJSON_Delete(root);
+
     fflush(stderr);
     fflush(stdout);
     exit(return_code);

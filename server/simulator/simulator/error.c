494601ac92342ac1af7da5a62a1d0efd927efd4f
Karam Singh <karamsingh175@gmail.com>
2019-06-10 12:17:45 -0400

    changed json output for error

--- server/simulator/simulator/error.c
+++ server/simulator/simulator/error.c
@@ -9,7 +9,7 @@ void error(char *error_msg, int return_code) {
     
     cJSON *root = cJSON_CreateObject();
     cJSON *msg = cJSON_CreateString(error_msg);
-    cJSON_AddItemToObject(root, "msg", msg);
+    cJSON_AddItemToObject(root, "error", msg);
     cJSON_AddNumberToObject(root, "code", return_code);
     fprintf(stderr, "%s", cJSON_Print(root));
     cJSON_Delete(root);

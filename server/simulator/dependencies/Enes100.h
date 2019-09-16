c6d08acbbcce6301200f8a2004f4961f4f306ebe
dotcomstar <gamestar109@live.com>
2019-09-16 03:52:15 -0400

    Update Enes100.h library

--- server/simulator/dependencies/Enes100.h
+++ server/simulator/dependencies/Enes100.h
@@ -1,28 +1,26 @@
-#ifndef ENES100_H
-#define ENES100_H
+#ifndef Enes100_h
+#define Enes100_h
 
 #include "VisionSystemClient.hpp"
-#include "ArduinoHelpers.hpp"
 
-#define INPUT 0
-#define OUTPUT 0
-#define LOW 0
-#define HIGH 0
+#define BLACK_BOX         0
+#define DATA              1
+#define DEBRIS            2
+#define FIRE              3
+#define WATER             4
 
-#define BLACK_BOX 0
-#define CHEMICAL 1
-#define DEBRIS 2
-#define FIRE 3
-#define WATER 4
+#define RED               0
+#define GREEN             1
+#define BLUE              2
 
-#define FRESH 0
-#define POLLUTED 1
-#define SALT 2
+#define FRESH_UNPOLLUTED  0
+#define FRESH_POLLUTED    1
+#define SALT_UNPOLLUTED   2
+#define SALT_POLLUTED     3
 
-#define COPPER 0
-#define STEEL 1
+#define PLASTIC           0
+#define ALUMINUM          1
 
 VisionSystemClient Enes100;
-SerialClient Serial;
 
-#endif
+#endif /* Enes100_h */

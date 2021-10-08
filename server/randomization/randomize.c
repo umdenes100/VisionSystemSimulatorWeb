1c81085eb2c397117867ab34b7e5426bc3c969b1
Gary <gsneed@umd.edu>
2021-10-08 16:07:43 -0400

    adding randomization changes from actual vision system

--- server/randomization/randomize.c
+++ server/randomization/randomize.c
@@ -63,6 +63,69 @@ void randomize(void) {
     int i, randomization = rand() % 6;
     float baseY, xMin, xMax, yMin, yMax, destX, destY;
 
+
+
+
+
+    int flipFlop = rand() % 2;
+    int randColor = rand() % 2;
+    //mStartingLocation.x = 0.55;
+    //mStartingLocation.y = 0.55 + (flipFlop) * 0.9;  
+
+    //generate starting location
+    root = cJSON_CreateObject();
+    cJSON_AddItemToObject(root, "type", cJSON_CreateString("randomization"));
+    cJSON_AddItemToObject(root, "osv", osv = cJSON_CreateObject());
+    cJSON_AddNumberToObject(osv, "x", 0.55);
+    cJSON_AddNumberToObject(osv, "y", 0.55 + (flipFlop)*0.9);
+    //cJSON_AddNumberToObject(osv, "theta", (rand() % 2) * PI - PI/2);
+
+    //generate destination
+    dest = cJSON_CreateObject();
+    cJSON_AddNumberToObject(dest, "x", 0.55);
+    cJSON_AddItemToObject(root, "destination", dest);
+
+    static int presets[4][3] = {
+        {0, 2},
+        {1, 2},
+        {2, 0},
+        {2, 1}
+    };
+
+    if(flipFlop) {
+	cJSON_AddNumberToObject(dest, "y", 0.55);
+        cJSON_AddNumberToObject(osv, "theta", (rand() % 180) * 2 * PI / 360);
+	presets[0][0] = 0;
+        presets[0][1] = 2;
+
+        presets[1][0] = 1;
+        presets[1][1] = 2;
+
+        presets[2][0] = 2;
+        presets[2][1] = 0;
+
+        presets[3][0] = 2;
+        presets[3][1] = 1;
+    } else {
+	cJSON_AddNumberToObject(dest, "y", 1.45);
+	cJSON_AddNumberToObject(osv, "theta", ((rand() % 180)+180) * 2 * PI / 360);
+        presets[0][0] = 0;
+        presets[0][1] = 1;
+
+        presets[1][0] = 0;
+        presets[1][1] = 2;
+
+        presets[2][0] = 1;
+        presets[2][1] = 0;
+
+        presets[3][0] = 2;
+        presets[3][1] = 0;
+
+    }
+
+    cJSON_AddItemToObject(root, "destination", dest);
+
+/*
     static const int presets[6][2] = {
         {0, 1},
         {0, 2},
@@ -80,13 +143,14 @@ void randomize(void) {
     cJSON_AddNumberToObject(osv, "x", 0.35);
     cJSON_AddNumberToObject(osv, "y", 0.4 + (rand() % 4)*0.4);
     cJSON_AddNumberToObject(osv, "theta", (rand() % 2) * PI - PI/2);
+*/
 
     //generate obstacles
     obstacles = cJSON_CreateArray();
     for(int i = 0; i < 2; i++) {
-        baseY = presets[randomization][i] * 0.65 + OBSTACLE_HEIGHT + 0.1;
+        baseY = presets[randomization][i] * 0.5 + OBSTACLE_HEIGHT + 0.1;
         point = cJSON_CreateObject();
-        cJSON_AddNumberToObject(point, "x", i * 0.7 + 1.8);
+        cJSON_AddNumberToObject(point, "x", i * 0.8 + 1.5);
         cJSON_AddNumberToObject(point, "y", baseY);
         cJSON_AddNumberToObject(point, "width", 0.2);
         cJSON_AddNumberToObject(point, "height", 0.5);
@@ -94,6 +158,8 @@ void randomize(void) {
     }
     cJSON_AddItemToObject(root, "obstacles", obstacles);
 
+
+/*
     //generate destination
     xMin = 2.8 + 0.4 + TARGET_DIAMETER / 2;
     xMax = 4 - OSV_WIDTH - 0.1 - TARGET_DIAMETER / 2;
@@ -107,6 +173,7 @@ void randomize(void) {
     cJSON_AddNumberToObject(dest, "x", destX);
     cJSON_AddNumberToObject(dest, "y", destY);
     cJSON_AddItemToObject(root, "destination", dest);
+*/
 
     if (print_preallocated(root) != 0) {
         cJSON_Delete(root);
